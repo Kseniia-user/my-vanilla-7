b894adcb0328507ecbdc88b1d4cb4131210e3437
Kseniia-user <110529101+Kseniia-user@users.noreply.github.com>
2022-10-02 20:16:50 +0300

    Added relevant icon from the API

--- index.js
+++ index.js
@@ -35,19 +35,26 @@ function formatDate(timestamp) {
 }
 
 function displayTemperature(response) {
-  console.log(response.data.main.temp);
+  console.log(response.data);
   let temperatureElement = document.querySelector(#temperature);
   let cityElement = document.querySelector(#city);
   let descriptionElement = document.querySelector(#description);
   let humidityElement = document.querySelector(#humidity);
   let windElement = document.querySelector(#wind);
   let dateElement = document.querySelector(#date);
+  let iconElement = document.querySelector(#icon);
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
   descriptionElement.innerHTML = response.data.weather[0]description;
   humidityElement.innerHTML = response.data.main.humidity;
   windElement.innerHTML = wath.round(response.data.wind.speed);
   dateElement.innerHTML = formatDate(response.data.dt * 1000);
+  iconElement.setAttribute(
+    "src", 
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+    );
+  iconElement.setAttribute(
+    "alt", response.data.weather[0]description);
 } 
 
 function searchCity(city) {
@@ -77,7 +84,8 @@ function handleSubmit(event) {
 
 function searchLocation(position) {
   let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
+  let city = "Odessa";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${city}${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
 
   axios.get(apiUrl).then(displayWeatherCondition);
 }
