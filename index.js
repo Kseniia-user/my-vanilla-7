83478d4fe427ff579e66dd7bdde665167e4350ec
Kseniia-user <110529101+Kseniia-user@users.noreply.github.com>
2022-10-05 01:07:00 +0300

    Remowed the imperial conversion 👋

--- index.js
+++ index.js
@@ -19,11 +19,62 @@ function formatDate(timestamp) {
     "Saturday"
   ];
   let day = days[date.getDay()];
-
   return `${day} ${hours}:${minutes}`;
 
+function getForecast(response) {
+  console.log(response.data.daily);
+  let apiKey = "8d9838178b5b401f1b4e7cb5af18e210";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&exclude={part}&appid=${apiKey}&units=metric`;
+  
+  axios.get(apiUrl).then(displayForecast);
+}
+
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+ 
+  return days[day];
+}
+
+function displayForecast(response) {
+  let forecast = response.data.daily;
+  
+  let forecastElement = document.querySelector("#forecast");
+
+ 
+  let forecastHTML = `<div class=row>`;
+
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 5) {
+  forecastHTML = 
+    forecastHTML +
+   `
+  <div class="col=2">
+    <div class="weather-forecast-date">${forecastDay(forecastDay.dt)}</div>
+      <img 
+        src="http://openweathermap.org/imp/wn/50d${forecastDay.weather[0].icon}@2x.png" 
+        alt=""
+        width="42"
+      />
+      <div class="weather-forecast-temperatures">
+       <span class="weather-forecast-temperature-max">
+        ${Math.round(forecastDay.temp.max)}° </span> 
+       <span class="weather-forecast-temperature-min">
+        ${Math.round(forecastDay.temp.min)}° </span>
+      </div>
+    </div>
+  </div>
+  `;
+    }
+  });
+
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+  console.log(forecastHTML);
+} 
+
 function displayTemperature(response) {
-  console.log(response.data);
   let temperatureElement = document.querySelector(#temperature);
   let cityElement = document.querySelector(#city);
   let descriptionElement = document.querySelector(#description);
@@ -36,22 +87,20 @@ function displayTemperature(response) {
 
   temperatureElement.innerHTML = Math.round(celsiusTemperature);
   cityElement.innerHTML = response.data.name;
-  descriptionElement.innerHTML = response.data.weather[0]description;
+  descriptionElement.innerHTML = response.data.weather[0].description;
   humidityElement.innerHTML = response.data.main.humidity;
   windElement.innerHTML = wath.round(response.data.wind.speed);
   dateElement.innerHTML = formatDate(response.data.dt * 1000);
   iconElement.setAttribute(
     "src", 
-    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
-    );
-  iconElement.setAttribute(
-    "alt", response.data.weather[0]description);
+    `http://openweathermap.org/img/wn/${response.data.weather[0](description);<SVGAnimatedStringsad>sad
+  sad
+  </SVGAnimatedStringsad>
 } 
 
-function searchCity(city) {
-  let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${Odessa}&appid=${apiKey}&units=metric`;
-  axios.get(apiUrl).then(displayWeatherCondition);
+function search(city) {
+  let apiKey = "8d9838178b5b401f1b4e7cb5af18e210";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayTemperature);
 }
 
@@ -61,72 +110,7 @@ function handleSubmit(event){
   search(cityInputElement.value);
 }
 
-function displayFahrenheitTemperature(event){
-  event.preventDefault();
-  let temperatureElement = document.querySelector("#temperature");
-  
-  celsiusLink.classList.remove("active");
-  fahrenheitLink.classList.add("active");
-  let fahrenheitTemperature = (celsiusTemperature * 9) / 5 + 32;
-  temperatureElement.innerHTML = Math.round(fahrenheitTemperature);
-}
-function displayCelsiusTemperature(event){
-  event.preventDefault();
-  celsiusLink.classList.add("active");
-  fahrenheitLink.classList.remove("active");
-  let temperatureElement = document.querySelector("#temperature");
-  temperatureElement.innerHTML = Math.round(celsiusTemperature);
-}
-let celsiusTemperature = null;
-
 let forn = document.querySelector("#search-forn");
 forn.addEventListener("submit", handleSubmit);
 
-let fahrenheitLink = document.querySelector("#fahrenheit-link");
-fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
-
-let celsiusLink = document.querySelector("#celsius-link");
-celsiusLink.addEventListener("click", displayCelsiusTemperature);
-
 search("Odessa");
-
-
-
-function displayWeatherCondition(response) {
-  document.querySelector("#city").innerHTML = response.data.name;
-  document.querySelector("#temperature").innerHTML = Math.round(response.data.main.temp);
-
-  document.querySelector("#humidity").innerHTML = response.data.main.humidity;
-  document.querySelector("#wind").innerHTML = Math.round(
-    response.data.wind.speed
-  );
-  document.querySelector("#description").innerHTML =
-    response.data.weather[0].main;
-}
-
-function searchLocation(position) {
-  let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
-  let city = "Odessa";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${city}${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
-
-  axios.get(apiUrl).then(displayWeatherCondition);
-}
-
-function getCurrentLocation(event) {
-  event.preventDefault();
-  navigator.geolocation.getCurrentPosition(searchLocation);
-}
-
-
-let dateElement = document.querySelector("#date");
-let currentTime = new Date();
-dateElement.innerHTML = formatDate(currentTime);
-
-let searchForm = document.querySelector("#search-form");
-searchForm.addEventListener("submit", handleSubmit);
-
-let currentLocationButton = document.querySelector("#current-location-button");
-currentLocationButton.addEventListener("click", getCurrentLocation);
-
-
-
